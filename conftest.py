6516c1a69690133025e916e6884f3407ead23245
Marcos Ramiro <mrsantos2006@gmail.com>
2022-03-07 23:51:08 -0300

    primeiro commit

---
+++ conftest.py
@@ -0,0 +1,43 @@
+from pytest import fixture
+import requests
+import json
+import os
+
+
+headers = {
+    'Authorization': 'Bearer sdfgdkgjsgsjdlsdfkljdk',
+    'x-bla': 'bla'
+}
+
+
+@fixture(scope='session')
+def environment():
+    try:
+        url = os.getenv('URL_STAR_WARS', 'https://swapi.dev/api/people/1')
+        client_id = os.getenv('CLIENT_ID', 'minha_client_id')
+        client_secret = os.getenv('CLIENT_SECRET', 'minha_client_secret')
+        yield url, client_id, client_secret
+    finally:
+        print('fim do metodo environment fixture')
+
+
+@fixture(scope='session')
+def token(environment):
+    """call sts to generate token"""
+    return "fake_token"
+
+
+@fixture
+def url(environment):
+    return environment[0]
+
+
+@fixture
+def get_api_start_wars_people_one(url, token):
+    print('entrei no get_start_wars_people_one fixture')
+    headers.update({'Authorization': f'Bearer {token}'})
+    print(headers)
+    resposta_api = requests.get(url, headers=headers).text
+    resposta_json = json.loads(resposta_api)
+    return resposta_json
+
---
+++ test_api.py
@@ -0,0 +1,12 @@
+from conftest import get_api_start_wars_people_one
+
+
+def test_deve_conter_nome_do_Luky(get_api_start_wars_people_one):
+    name = get_api_start_wars_people_one['name']
+    assert name == 'Luke Skywalker'
+
+
+def test_deve_ter_participado_de_quatro_filmes(get_api_start_wars_people_one):
+    films = get_api_start_wars_people_one['films']
+    assert len(films) == 4
+
